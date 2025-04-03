be8dc4351c46ba4a8809db2598e8f9d54146b086
vtal999 <klvitaly09@gmail.com>
2025-04-03 10:13:03 +0300

    Update update_playlist.py
    5

--- update_playlist.py
+++ update_playlist.py
@@ -103,7 +103,13 @@ def update_playlist(video_urls):
 # Основная функция
 def main():
     channels = {
-        "Сапфир": "https://onlinetv.su/tv/kino/262-sapfir.html",
+         "2+2": "http://ip.viks.tv/114427-22-tv.html",
+        "СТБ": "http://ip.viks.tv/032117-stb.html",
+        "Первый канал": "http://ip.viks.tv/021612-pervyy-kanal.html",
+        "Россия 1": "http://ip.viks.tv/031327-rossiya1_tv.html",
+        "Россия 24": "http://ip.viks.tv/126307-rossiya_24_tv.html",
+        "НСТ": "http://ip.viks.tv/394-nst_4.html",
+        "Кинохит": "http://ip.viks.tv/477-kinohit_14.html",
     }
 
     video_urls = {}
